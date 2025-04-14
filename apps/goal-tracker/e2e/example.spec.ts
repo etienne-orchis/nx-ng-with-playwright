0c54d095bb997745f0ff7e8e468c1bf0153782e3
etienne-orchis <etienne.razafindramiarina@orchisintl.com>
2025-04-14 10:10:32 +0300

    using another test run

--- apps/goal-tracker/e2e/example.spec.ts
+++ apps/goal-tracker/e2e/example.spec.ts
@@ -1,25 +1,19 @@
+/* eslint-disable playwright/valid-title */
 import { test, expect } from '@playwright/test';
 
-test('TR0001 - QA Touch home page title rendering', async ({ page }) => {
-  await page.goto('https://www.qatouch.com/');
-  await expect(page).toHaveTitle(
-    /Comprehensive Test Management Tool For Small QA Teams/
-  );
-});
-
-test('TR0002 - Has the right app title', async ({ page }) => {
-  await page.goto('/');
-  await expect(page).toHaveTitle(/Goal tracker/);
-});
-
-test('TR0003 - Test with steps', async () => {
-  await test.step('TR0003 - First step', async () => {
+test('TR0006 - Test with steps', async () => {
+  await test.step('S1 - First step', async () => {
     const result = 1; // Simulating a value
     expect(result).toBe(1); // Assertion for expected result
   });
 
   // Step 2: Second step, expect result to be 2
-  await test.step('TR0003 - Second step', async () => {
+  await test.step('S2 - Second step', async () => {
+    const result = 2; // Simulating another value
+    expect(result).toBe(2); // Assertion for expected result
+  });
+
+  await test.step('S3 - Third step', async () => {
     const result = 2; // Simulating another value
     expect(result).toBe(2); // Assertion for expected result
   });
